4fd02be910125821a18c6b4fd455cab5a3acb643
rscherrer <raphael.scherrer@evobio.eu>
2020-04-06 17:04:17 +0200

    done with making plots

--- adaptive_dynamics.R
+++ adaptive_dynamics.R
@@ -3,66 +3,146 @@ rm(list = ls())
 # Here we make figures to show the outcome of the adaptive dynamics across parameter
 # space, from data generated in Mathematica.
 
+#### Accessory functions ####
+
+mathematica2r <- function(data, numerics, digits = 4) {
+  
+  library(tidyverse)
+  
+  data %>% 
+    mutate_if(~ any(.x == "True"), ~ .x == "True") %>%
+    mutate_at(numerics, ~ round(as.numeric(as.character(.x)), digits))
+  
+}
+
+predict_adaptive_dynamics <- function(data, xstart) {
+  
+  library(tidyverse)
+  
+  data %>% mutate(
+    isbp = isconv & curv > 0,
+    isabove = x > xstart,
+    isbelow = x <= xstart,
+    distance = abs(x - xstart)
+  ) %>%
+    summarize(
+      
+      # Is there any branching point?
+      anybp = any(isbp), 
+      
+      # Find the closest singularities surrounding the starting point
+      anyabove = any(isabove), 
+      anybelow = any(isbelow),
+      iabove = ifelse(anyabove, which(isabove & distance == min(distance[isabove])), NA),
+      ibelow = ifelse(anybelow, which(isbelow & distance == min(distance[isbelow])), NA),
+      
+      # Which singularities are convergence stable?
+      isconvabove = ifelse(anyabove, isconv[iabove], NA),
+      isconvbelow = ifelse(anybelow, isconv[ibelow], NA),
+      ireached = ifelse(anyabove, ifelse(isconvabove, iabove, ifelse(anybelow, ifelse(isconvbelow, ibelow, NA), -Inf)), ifelse(anybelow, ifelse(isconvbelow, ibelow, Inf), NA)),
+      
+      # Which are reached and are they evolutionarily stable?
+      xreached = ifelse(ireached %in% c(NA, -Inf, Inf), ireached, x[ireached]),
+      conv = ifelse(ireached %in% c(NA, -Inf, Inf), NA, isconv[ireached]),
+      inv = ifelse(ireached %in% c(NA, -Inf, Inf), NA, curv[ireached] > 0)
+      
+    )
+}
+
+make_facet_labels <- function(data, varname) {
+  
+  labs <- unique(unlist(data[, varname]))
+  labnames <- as.character(labs)
+  labs <- paste(varname, labs, sep = " = ")
+  names(labs) <- labnames
+  return (labs)
+  
+}
+
+plot_divergence_map <- function(data, xstart, keep_legend = TRUE) {
+  
+  library(tidyverse)
+  library(cowplot)
+  
+  # Summarize the adaptive dynamics given a starting point
+  smr <- data %>% 
+    
+    # Evaluate the attained equilibrium for each parameter set in each patch
+    group_by(a, h, b, s, d, alpha, patch) %>% 
+    predict_adaptive_dynamics(xstart = xstart) %>%
+    select(a, h, b, s, d, alpha, patch, anybp, xreached, conv, inv) %>%
+    mutate(branching = conv & inv) %>% 
+    
+    # Calculate the difference between patches
+    group_by(a, h, b, s, d, alpha) %>%
+    summarize(
+      diff = dist(xreached), # distance in equilibria reached
+      bothbranch = all(branching) # does branching occur in both patches?
+    ) %>%
+    filter(!is.na(diff))
+  
+  # Plot the divergence in evolutionary outcome between the patches
+  p <- ggplot(smr, aes(x = h, y = s, fill = diff)) + 
+    geom_tile() +
+    scale_fill_gradient(low = "black", high = "lightblue") +
+    theme_bw() +
+    ylab("Ecological divergent selection") +
+    xlab("Habitat symmetry") +
+    labs(fill = "Allopatric\ndivergence") +
+    xlim(c(0, 1)) +
+    
+    # Delineate where within-patch branching occurs
+    geom_polygon(data = smr %>% 
+                   filter(bothbranch) %>%
+                   group_by(s) %>%
+                   summarize(h = min(h)) %>%
+                   add_row(s = max(smr$s[smr$bothbranch]), h = max(smr$h), .before = TRUE) %>%
+                   add_row(s = min(smr$s[smr$bothbranch]), h = max(smr$h), .after = TRUE), 
+                 aes(x = h, y = s, fill = NULL), alpha = 0.3, fill = "yellow", color = "black", lty = 3) +
+    theme(legend.key.height = unit(7, "mm"), legend.margin = margin(0, 0, 0, 0))
+  
+  # Manipulate legends
+  leg1 <- get_legend(p)
+  leg2 <- get_legend(
+    ggplot(data.frame(x = 1, y = 1, log = "Sympatric\nbranching"), aes(x = x, y = y, alpha = log)) + 
+      geom_tile(fill = "yellow") + 
+      scale_alpha_manual(values = 0.3) + 
+      labs(alpha = NULL) + 
+      theme(legend.margin = margin(0, 0, 30, 0))
+  )
+  legs <- plot_grid(leg1, leg2, nrow = 2, rel_heights = c(2, 1))
+  
+  p <- p + theme(legend.position = "none")
+  
+  if (!keep_legend) return (p)
+  
+  plot_grid(p, legs, ncol = 2, rel_widths = c(2, 1))  
+  
+}
+
+
+#### Analysis ####
+
 library(tidyverse)
-library(cowplot)
 
 # Load the data
-data1 <- read.csv("data/adaptive_dynamics_m0.001.csv", header = TRUE)
-data2 <- read.csv("data/adaptive_dynamics_m0.01.csv", header = TRUE)
-data3 <- read.csv("data/adaptive_dynamics_m0.1.csv", header = TRUE)
-data <- rbind(data1, data2, data3)
-rm(data1, data2, data3)
-
-# A few corrections
-data$isconv <- data$isconv == "True"
-data[, c("x", "N1", "N2", "curv")] <- sapply(data[, c("x", "N1", "N2", "curv")], function(x) as.numeric(as.character(x)))
-data[, c("a", "b", "h", "s", "d", "m", "alpha")] <- sapply(data[, c("a", "b", "h", "s", "d", "m", "alpha")], round, 3)
-
-head(data)
-
-# Starting trait value (to evaluate whether a branching point can be reached)
-x0 <- -1 
+numerics <- c("a", "b", "h", "s", "d", "m", "alpha", "x", "N1", "N2", "curv")
+data <- do.call("rbind", list(
+  read.csv("data/adaptive_dynamics_m0.001.csv", header = TRUE) %>% mathematica2r(numerics = numerics),
+  read.csv("data/adaptive_dynamics_m0.01.csv", header = TRUE) %>% mathematica2r(numerics = numerics),
+  read.csv("data/adaptive_dynamics_m0.1.csv", header = TRUE) %>% mathematica2r(numerics = numerics)
+))
 
 # Each observation in the data is a singularity
 # Summarize the data per parameter set
-smr <- data %>% group_by(a, b, h, s, d, m, alpha) %>% 
-  mutate(
-    isbp = isconv & curv > 0,
-    isabove = x > x0,
-    isbelow = x <= x,
-    distance = abs(x - x0)
-  ) %>%
-  summarize(
-    
-    # Is there any branching point?
-    anybp = any(isbp), 
-    
-    # Find the closest singularities surrounding the starting point
-    anyabove = any(isabove), 
-    anybelow = any(isbelow),
-    iabove = ifelse(anyabove, which(isabove & distance == min(distance[isabove])), NA),
-    ibelow = ifelse(anybelow, which(isbelow & distance == min(distance[isbelow])), NA),
-    
-    # Which singularities are convergence stable?
-    isconvabove = ifelse(anyabove, isconv[iabove], NA),
-    isconvbelow = ifelse(anybelow, isconv[ibelow], NA),
-    ireached = ifelse(anyabove, ifelse(isconvabove, iabove, ifelse(anybelow, ifelse(isconvbelow, ibelow, NA), -Inf)), ifelse(anybelow, ifelse(isconvbelow, ibelow, Inf), NA)),
-    
-    # Which are reached and are they evolutionarily stable?
-    xreached = ifelse(ireached %in% c(NA, -Inf, Inf), ireached, x[ireached]),
-    conv = ifelse(ireached %in% c(NA, -Inf, Inf), NA, isconv[ireached]),
-    inv = ifelse(ireached %in% c(NA, -Inf, Inf), NA, curv[ireached] > 0)
-    
-  ) %>%
+smr <- data %>% group_by(a, b, h, s, d, m, alpha) %>%
+  predict_adaptive_dynamics(xstart = -1) %>%
   select(a, b, h, s, d, m, alpha, anybp, xreached, conv, inv) %>%
-  mutate(branching = conv & inv) %>%
-  mutate(whatbp = ifelse(anybp, ifelse(branching, "BP reachable", "BP exists"), "No BP"))
+  mutate(branching = conv & inv)
 
 # Facet labels
-m_labs <- paste("m =", unique(smr$m))
-names(m_labs) <- as.character(unique(smr$m))
-alpha_labs <- paste("alpha = ", unique(smr$alpha))
-names(alpha_labs) <- as.character(unique(smr$alpha))
+m_labs <- make_facet_labels(smr, "m")
+alpha_labs <- make_facet_labels(smr, "alpha")
 
 # Is there any branching point?
 p <- ggplot(smr %>% filter(anybp), aes(x = h, y = s, fill = anybp)) + 
@@ -133,34 +213,26 @@ ggsave("figures/map_branching_points.png", p, width = 7, height = 3, dpi = 300)
 
 # One patch model
 
-rm(list = ls())
 library(tidyverse)
+library(cowplot)
 
-data1 <- read.csv("data/adaptive_dynamics_patch1.csv")
-data2 <- read.csv("data/adaptive_dynamics_patch2.csv")
+numerics <- c("a1", "a2", "b", "s", "d", "alpha", "x", "N", "curv")
+data <- do.call(rbind, list(
+  read.csv("data/adaptive_dynamics_patch1.csv") %>% mathematica2r(numerics = numerics) %>% mutate(patch = 1),
+  read.csv("data/adaptive_dynamics_patch2.csv") %>% mathematica2r(numerics = numerics) %>% mutate(patch = 2)
+)) %>%
+  
+  # Sexual selection has no influence on the results here (probably can prove that)
+  filter(alpha == 0) %>%
+  
+  # Evaluate habitat symmetry
+  mutate(a = max(a1, a2), h = a1 / a2) %>%
+  mutate(h = replace(h, h > 1, (a2 / a1)[h > 1]))
 
-data1$isconv <- data1$isconv == "True"
-data1[, c("x", "N", "curv")] <- sapply(data1[, c("x", "N", "curv")], function(x) as.numeric(as.character(x)))
-data1[, c("a1", "a2", "b", "s", "d", "alpha")] <- sapply(data1[, c("a1", "a2", "b", "s", "d", "alpha")], round, 3)
+p1 <- plot_divergence_map(data, xstart = 0, keep_legend = FALSE)
+p2 <- plot_divergence_map(data, xstart = -1)
+p <- plot_grid(p1, p2, labels = c("A", "B"), rel_widths = c(0.8, 1.2))
+p 
 
-data2$isconv <- data2$isconv == "True"
-data2[, c("x", "N", "curv")] <- sapply(data2[, c("x", "N", "curv")], function(x) as.numeric(as.character(x)))
-data2[, c("a1", "a2", "b", "s", "d", "alpha")] <- sapply(data2[, c("a1", "a2", "b", "s", "d", "alpha")], round, 3)
+ggsave("figures/divergence_across_patches.png", p, width = 7, height = 3, dpi = 300)
 
-data1 %>% 
-  group_by(a2, s) %>% 
-  summarize(anybp = any(isconv & curv > 0)) %>% 
-  ggplot(aes(x = a2, y = s, fill = anybp)) + 
-  geom_tile() 
-
-data2 %>% 
-  group_by(a1, s) %>% 
-  summarize(anybp = any(isconv & curv > 0)) %>% 
-  ggplot(aes(x = a1, y = s, fill = anybp)) + 
-  geom_tile() 
-
-data1 %>% 
-  group_by(a2, s) %>% 
-  summarize(anybp = any(isconv & curv > 0)) %>% 
-  ggplot(aes(x = a2, y = s, fill = anybp)) + 
-  geom_tile() 
